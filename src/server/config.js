ee9d86221b83b58879ff31dde4c81cd213baea4f
Tali <talicabana@gmail.com>
2025-12-15 02:30:08 -0300

    di formato

--- src/server/config.js
+++ src/server/config.js
@@ -1,41 +1,41 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import dbConnection from './dbConfig.js';
-import reservasRoutes from '../routes/reservas.route.js'
-import productRoutes from '../routes/productos.routes.js'; 
+    import express from 'express';
+    import cors from 'cors';
+    import morgan from 'morgan';
+    import { dirname } from 'path';
+    import { fileURLToPath } from 'url';
+    import dbConnection from './dbConfig.js';
+    import reservasRoutes from '../routes/reservas.route.js'
+    import productRoutes from '../routes/productos.routes.js'; 
 
-export default class Server{
-    constructor(){
-        this.app = express();
-        this.port = process.env.PORT || 3001;
-        this.productsPath = '/api/products';
-        this.reservasPath = '/api/reservas'
-        this.conectarDB();
-        this.middlewares();
-        this.routes();
+    export default class Server{
+        constructor(){
+            this.app = express();
+            this.port = process.env.PORT || 3001;
+            this.productsPath = '/api/products';
+            this.reservasPath = '/api/reservas'
+            this.conectarDB();
+            this.middlewares();
+            this.routes();
+        }
+        
+        async conectarDB() {
+            await dbConnection();
+        }
+        middlewares() {
+            this.app.use(cors());
+            this.app.use(express.json());
+            this.app.use(morgan('dev'));
+            const __dirname = dirname(fileURLToPath(import.meta.url));
+            this.app.use(express.static(__dirname + '/../../public'));
+        }
+        routes() {
+            this.app.use(this.productsPath, productRoutes)
+            this.app.use(this.reservasPath, reservasRoutes)
+        }
+        listen() {
+            this.app.listen(this.port, () => console.info(`Servidor ejecutándose en: http://localhost:${this.port}`));
+        }
     }
-    
-    async conectarDB() {
-        await dbConnection();
-    }
-    middlewares() {
-        this.app.use(cors());
-        this.app.use(express.json());
-        this.app.use(morgan('dev'));
-        const __dirname = dirname(fileURLToPath(import.meta.url));
-        this.app.use(express.static(__dirname + '/../../public'));
-    }
-    routes() {
-        this.app.use(this.productsPath, productRoutes)
-        this.app.use(this.reservasPath, reservasRoutes)
-    }
-    listen() {
-        this.app.listen(this.port, () => console.info(`Servidor ejecutándose en: http://localhost:${this.port}`));
-    }
-}
 
 
 
